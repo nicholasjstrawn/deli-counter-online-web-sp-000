e1032a0423a91a12ec5398cbd19fa22d1cccc690
Nicholas Strawn <nicholasjstrawn@gmail.com>
2021-04-26 21:46:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,9 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
-  end
+    counter = "The line is currently:"
+    katz_deli.each_with_index do |value, index|
+      counter += " #{index.to_i+1}. #{value}"
+    end
+  puts "#{counter}"
 end
